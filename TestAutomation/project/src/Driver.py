662f01aae54f64143a52fc65c3164fc3c79152ad
Chandler DeLoach <deloachcd@g.cofc.edu>
2017-11-12 18:56:40 -0500

    Modified the driver, it's now ready for testing

--- TestAutomation/project/src/Driver.py
+++ TestAutomation/project/src/Driver.py
@@ -7,43 +7,53 @@ class Driver:
         self.testcase = testcase
 
     def execute(self):
-        # TODO: add sys.path specification
         t = self.testcase
-        exec("from %s import %s" % (t.component_provider, t.component_class))
-        #try:
-        #   exec("from %s import %s" % (t.component_provider, t.component_class))
-        #except ImportError:
-        #    print("Error in execution: class or component provider %s not found" % t.component_provider)
-        #    raise(ImportError)
-        #    return None
-        TEST_CLASS_INST = eval("%s(%s)" % (t.component_class, t.input_constructor))
-        #try:
-        #    TEST_CLASS_INST = eval("%s(%s)" % (t.component_class, t.input_constructor))
-        #except NameError:
-        #    print("Error in construction: class not found in file")
-        #    return None
-        #except TypeError:
-        #    print("Error in construction: invalid number of arguments to constructor")
-        #    return None
-        if t.oracle.find("Error") != -1:
-            # Expect an error
-            sequence='''
+        error = None
+        try:
+           exec("from %s import %s" % (t.component_provider, t.component_class))
+        except ImportError:
+            print("Error in import: class %s not found" % t.component_class)
+            error = "ImportError"
+        except ModuleNotFoundError:
+            print("Error in import: provider module %s not found" % t.component_provider)
+            error = "ModuleNotFoundError"
+        except:
+            print("Unexpected error encountered in import, congrats!")
+            raw = str(sys.exc_info()[0]).split()[1]
+            error = "UnexpectedImport" + raw[1:len(raw)-2]
+            print("Error: %s" % error)
+        if error is None:
             try:
-                METHOD_RETURN = TEST_CLASS_INST.%s(%s)
-                print(METHOD_RETURN)
-            catch %s:
-                print("Method %s from class %s raised error: %s")
-            '''
-            exec(sequence % (t.method, t.input_method, t.oracle, t.method, t.component_class, t.oracle))
-        else:
-            # Expect method return
+                TEST_CLASS_INST = eval("%s(%s)" % (t.component_class, t.input_constructor))
+            except NameError:
+                print("Error in construction: constructor for class %s not found" % t.component_provider)
+                error = "NameError"
+            except TypeError:
+                print("Error in construction: invalid number of arguments to constructor")
+                error = "TypeError"
+            except:
+                print("Unexpected error encountered in construction, congrats!")
+                raw = str(sys.exc_info()[0]).split()[1]
+                error = "UnexpectedConstruction" + raw[1:len(raw)-2]
+                print("Error: %s" % error)
+        if error is None:
             try:
                 METHOD_RETURN = eval("TEST_CLASS_INST.%s(%s)" % (t.method, t.input_method))
             except AttributeError:
-                print("Error: method %s not found in class" % t.method)
+                print("Error: method %s not found in class %s from provider %s" % t.method, t.component_class, t.component_provider)
+                error = "AttributeError"
+            except:
+                raw = str(sys.exc_info()[0]).split()[1]
+                error = raw[1:len(raw)-2]
+                print("Error: %s" % error)
+        if error is not None:
             print("Method %s from class %s returned %s" % (t.method, t.component_class, str(METHOD_RETURN)))
-        print("Oracle predicted %s" % str(t.oracle))
-        oracle_correct = eval("%s == %s" % (str(METHOD_RETURN), str(t.oracle)))
+            print("Oracle predicted %s" % str(t.oracle))
+            oracle_correct = eval("%s == %s" % (str(METHOD_RETURN), str(t.oracle)))
+        else:
+            print("Method %s from class %s raised %s" % (t.method, t.component_class, error))
+            print("Oracle predicted %s" % str(t.oracle))
+            oracle_correct = eval("%s == %s" % (error, str(t.oracle)))
         if oracle_correct:
             print("...and was correct!")
         else:
