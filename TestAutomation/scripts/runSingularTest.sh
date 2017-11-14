d658bb98a72fe61df8154c63f97deb9d9fa8a659
Chandler DeLoach <deloachcd@g.cofc.edu>
2017-11-13 21:14:30 -0500

    Added symlinks to script

--- TestAutomation/scripts/runSingularTest.sh
+++ TestAutomation/scripts/runSingularTest.sh
@@ -3,6 +3,6 @@
 source printlast
 TESTFILE="$1"
 SHORTNAME="`printlast $1`" 1>/dev/null # Get name from within testCases directory 
-../project/bin/DriverDriver.py "$TESTFILE" #> "../temp/$SHORTNAME"
+../project/bin/Loader.py "$TESTFILE" #> "../temp/$SHORTNAME"
 #./createReportActual.sh "../temp/$SHORTNAME"
 #rm "../temp/$SHORTNAME"
