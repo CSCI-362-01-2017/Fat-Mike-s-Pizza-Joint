28bacab659f1d9f9b2f9c7c9e767abd3d0c09638
Ethan <ethantaylor411@gmail.com>
2017-11-20 16:54:58 -0500

    created a directory for broken files to inject faults into

---
+++ TestAutomation/project/src/brokenPythonFiles/Driver.py
@@ -0,0 +1,69 @@
+# Actual Driver class for utilized by testing framework
+# Written by Chandler DeLoach
+import sys
+from TestCase import TestCase
+
+class Driver:
+    def __init__(self, testcase):
+        self.testcase = testcase
+
+    def execute(self):
+        t = self.testcase
+        MethodError = None
+        print("-----------")
+        print(self.testcase)
+        print("-----------")
+        try:
+           exec("from %s import %s" % (t.component_provider, t.component_class))
+        except ImportError:
+            print("Error in import: class %s or module %s not found" % (t.component_class, t.component_provider))
+            return "ImportError"
+        except ModuleNotFoundError:
+            print("Error in import: provider module %s not found" % t.component_provider)
+            return "ModuleNotFoundError"
+        except:
+            print("Unexpected error encountered in import, congrats!")
+            raw = str(sys.exc_info()[0]).split()[1]
+            DriverError = "UnexpectedImport" + raw[1:len(raw)-2]
+            print("Driver error: %s" % error)
+            return DriverError
+        try:
+            if t.input_constructor != "static":
+                TEST_CLASS_INST = eval("%s(%s)" % (t.component_class, t.input_constructor))
+        except NameError:
+            print("Error in construction: constructor for class %s not found" % t.component_provider)
+            return "NameError"
+        except TypeError:
+            print("Error in construction: invalid number of arguments to constructor")
+            return "TypeError"
+        except:
+            print("Error in construction: genuinely unexpected Driver error")
+            raw = str(sys.exc_info()[0]).split()[1]
+            DriverError = "UnexpectedConstruction" + raw[1:len(raw)-2]
+            print("Driver error: %s" % DriverError)
+            return DriverError
+        try:
+            if t.input_constructor == "static":
+                METHOD_RETURN = str(eval("%s.%s(%s)"
+                                        % (t.component_class, t.method, t.input_method)))
+            else:
+                METHOD_RETURN = str(eval("TEST_CLASS_INST.%s(%s)" % (t.method, t.input_method)))
+        except AttributeError:
+            print("Error in exectuion: method %s not found in class %s from provider %s"
+                  % (t.method, t.component_class, t.component_provider))
+            return "AttributeError"
+        except:
+            raw = str(sys.exc_info()[0]).split()[1]
+            MethodError = raw[1:len(raw)-2]
+        if MethodError is None:
+            print("Method : %s\nInput : %s\nClass : %s\nReturn : %s\nOracle : %s"
+                  % (t.method, t.input_method, t.component_class, METHOD_RETURN, t.oracle))
+            oracle_correct = eval("\"%s\" == \"%s\"" % (METHOD_RETURN, t.oracle))
+        else:
+            print("Method : %s\nInput : %s\nClass : %s\nError : %s\nOracle : %s"
+                  % (t.method, t.input_method, t.component_class, MethodError, t.oracle))
+            oracle_correct = eval("\"%s\" == \"%s\"" % (MethodError, t.oracle))
+        if oracle_correct:
+            print("PASS")
+        else:
+            print("FAIL")
---
+++ TestAutomation/project/src/brokenPythonFiles/TestCase.py
@@ -0,0 +1,128 @@
+# TestCase class for containerizing data read from specification files
+#
+# Written by Chandler DeLoach
+class TestCase:
+    @staticmethod
+    def extract_contents(line):
+        full = line.split()
+        token = full[0]
+        error = None
+        if token.find(":") != -1:
+            token = token[:len(token)-1] # "Normalize" token by removing ':'
+            fi = 1 # 'field index' = index for start of field
+        else:
+            try:
+                colon = full[1]
+            except IndexError:
+                error = "SpecSyntaxError" # No text or ':'
+                return error
+            if colon.find(":") == -1:
+                error = "SpecSyntaxError" # They forgot the ':' before the text
+                return error
+            else:
+                fi = 2 
+        if token != "component" and token != "input":
+            try:
+                field = " ".join(full[fi:])
+            except IndexError:
+                error = "NullFieldError"
+            if field == "":
+                error = "NullFieldError"
+        else:
+            field = ""
+        if error is not None:
+            return error
+        else:
+            return [ token, field ]
+    
+    @staticmethod
+    def store_value(token, field, fieldmap):
+        for bucket in fieldmap:
+            if token == bucket[0]:
+                bucket[1] = field
+                bucket[2] += 1
+                return None
+        return "InvalidSpecTypeError" # If they got here, the spec type was invalid
+
+    @staticmethod
+    def parse_syntax(testCaseFileName):
+        fieldmap = [ ["name", "", 0],
+                     ["requirement", "", 0],
+                     ["method", "", 0],
+                     ["component_class", "", 0],
+                     ["component_provider", "", 0],
+                     ["input_constructor", "", 0],
+                     ["input_method", "", 0],
+                     ["oracle", "", 0]] # [name, field, incidences]
+        token_dict = { "constructor": "input_constructor",
+                       "method": "input_method",
+                       "class": "component_class",
+                       "provider": "component_provider" }
+        error = None
+        state_counter = 0 # number of upcoming lines to expect sub-specifications
+        with open(testCaseFileName, "r") as testCaseFile:
+            for line in testCaseFile:
+                line_content = TestCase.extract_contents(line)
+                if type(line_content) is str: # we know it's an error string and not a list
+                    error = line_content
+                    break
+                else:
+                    token = line_content[0]
+                    field = line_content[1]
+                if state_counter > 0:
+                    try:
+                        token = token_dict[token]
+                    except KeyError:
+                        error = "InvalidSubSpecTypeError"
+                        break
+                    state_counter -= 1
+                if token == "component" or token == "input":
+                    state_counter = 2
+                else:
+                    if token == "method" and field.find("()") != -1:
+                        field = field[:len(field)-2] # "Normalize" method_name by removing '()'
+                    elif token == "component_provider" and field.find(".py") != -1:
+                        field = field[:len(field)-3] # "Normalize" method_name by removing '()'
+                    error = TestCase.store_value(token, field, fieldmap) # will remain None if no error
+                    if error is not None:
+                        break
+        errorList = { "SpecSyntaxError": "invalid syntax in one or more specifications",
+                      "InvalidSpecTypeError": "invalid specification type for test case",
+                      "ConflictingSpecError": "one or more conflicting specifications within test case",
+                      "MissingSpecError": "test case is missing a required specification",
+                      "InvalidSubSpecTypeError": "invalid sub specification type",
+                      "NullFieldError": "field for one or more specifications is empty" }
+        if error is None:
+            for bucket in fieldmap:
+                if bucket[2] > 1:
+                    error = "ConflictingSpecError"
+                elif bucket[2] == 0:
+                    error = "MissingSpecError"
+                    print("Missing spec: %s" % bucket[0])
+        if error is not None:
+            print("Error in file: %s" % errorList[error])
+            return None
+        else:
+            for i in range(5,7):
+                if fieldmap[i][1] == "void":
+                    fieldmap[i][1] = ""
+            blueprint = []
+            for field in fieldmap:
+                blueprint.append(field[1]) # structure is implicit in ordering
+            return blueprint
+    
+    def __str__(self):
+        return "Name: " + self.name + "\n" + "Requirement: " + self.requirement + "\n" + "Method: " + self.method + "\n" + "Class: " + self.component_class + "\n" + "Provider: " + self.component_provider + "\n" + "Constructor: " + self.input_constructor + "\n" + "Method Input: " + self.input_method + "\n" + "Oracle: " + self.oracle
+
+    def __init__(self, blueprint):
+        if blueprint is not None:
+            self.name               = blueprint[0]
+            self.requirement        = blueprint[1]
+            self.method             = blueprint[2]
+            self.component_class    = blueprint[3]
+            self.component_provider = blueprint[4].replace("/", ".")
+            self.input_constructor  = blueprint[5]
+            self.input_method       = blueprint[6]
+            self.oracle             = blueprint[7]
+        else:
+            print("Error: attempted to create TestCase from improperly validated file!")
